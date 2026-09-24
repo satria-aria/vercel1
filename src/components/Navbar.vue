b0acf4d0bc183774dbcec1ec27311d48239b2f52
satria-aria <satriaaaw24@gmail.com>
2026-09-24 08:05:32 +0700

    main

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,31 @@
 <template>
       <nav>
         <h2>NewJeans Official</h2>
-        <RouterLink to="/">Landing</RouterLink> |
+        <div class="menu">
+       | <RouterLink to="/">Landing</RouterLink> |
         
           <RouterLink to="/product">Product</RouterLink> |
          <RouterLink to="/about">About</RouterLink> |
            <RouterLink to="/contact">Contact</RouterLink> |
            <RouterLink to="/testimoni">Testimoni</RouterLink> |
+           <RouterLink to="/NewJeans">NewJeans</RouterLink> |
+           </div>
       </nav>
   <hr />
 </template>
+<style>
+nav {
+  position:relative;
+  display: flex;
+  align-items: center;
+  padding: 15px 30px;
+}
+
+.menu {
+  position: absolute;
+  left: 50%;
+  transform: translatex(-50%);
+  display: flex;
+  gap: 30px;
+}
+</style>
